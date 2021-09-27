2a969925cae8bdefc3198ffde0eea42d37b3af99
arjunrvan <arjunrvan@gmail.com>
2021-09-27 15:12:05 +0800

    update parallax effect

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ var layerTwo = document.getElementById("layerTwo");
 var layerThree = document.getElementById("layerThree");
 var layerFour = document.getElementById("layerFour");
 var layerFive = document.getElementById("layerFive");
+var layerSix = document.getElementById("layerSix");
+var layerSeven = document.getElementById("layerSeven");
+var layerEight = document.getElementById("layerEight");
+var layerNine = document.getElementById("layerNine");
+var layerTen = document.getElementById("layerTen");
+var layerEleven = document.getElementById("layerEleven");
 
-var layerList = document.querySelectorAll('.layer');
+// var layerList = document.querySelectorAll('.layer');
 
 var container1Pos = {w: 800, h: 600};
+var container2Pos = {w: 800, h: 600};
 var layerOnePos = {x: 0, y: 0, w:1000, h: 600};
 var layerTwoPos = {x: 0, y: 0, w: 1000, h: 600};
 var layerThreePos = {x: 0, y: 0, w: 1000, h: 600};
 var layerFourPos = {x: 0, y: 0, w: 1000, h: 600};
 var layerFivePos = {x: 0, y: 0, w: 1000, h: 1000};
+var layerSixPos = {x: 0, y: 0, w:1000, h: 600};
+var layerSevenPos = {x: 0, y: 0, w: 1000, h: 600};
+var layerEightPos = {x: 0, y: 0, w: 1000, h: 600};
+var layerNinePos = {x: 0, y: 0, w: 1000, h: 600};
+var layerTenPos = {x: 0, y: 0, w: 1000, h: 600};
+var layerElevenPos = {x: 0, y: 0, w: 1000, h: 600};
 var mouse = {x: 0, y:0};
 
 // Ensure bring page to top when refreshed
@@ -24,8 +37,8 @@ window.onbeforeunload = function() {
 window.addEventListener("mousemove", onMouseMove);
 
 function onMouseMove (evt) {
-    mouse.x=  evt.pageX - container.offsetLeft;
-    mouse.y = evt.pageY - container.offsetTop;
+    mouse.x=  evt.pageX - container1.offsetLeft;
+    mouse.y = evt.pageY - container1.offsetTop;
 
     layerOnePos.x = (-mouse.x* 0.1) + (container1Pos.w/2 - layerOnePos.w / 2) ;
     layerOne.style.left = layerOnePos.x + 'px';
@@ -43,6 +56,28 @@ function onMouseMove (evt) {
     layerFivePos.x = (-mouse.x* 0.006) + (container1Pos.w/2 - layerFivePos.w / 2) ;
     layerFive.style.left = layerFivePos.x + 'px';
 
+    // Container 2
+    mouse.x=  evt.pageX - container2.offsetLeft;
+    mouse.y = evt.pageY - container2.offsetTop;
+
+    layerSixPos.x = (-mouse.x* 0.2) + (container2Pos.w/2 - layerSixPos.w / 2) ;
+    layerSix.style.left = layerSixPos.x + 'px';
+    
+    layerSevenPos.x = (-mouse.x* 0.15) + (container2Pos.w/2 - layerSevenPos.w / 2) ;
+    layerSeven.style.left = layerSevenPos.x + 'px';
+
+    layerEightPos.x = (-mouse.x* 0.12) + (container2Pos.w/2 - layerEightPos.w / 2) ;
+    layerEight.style.left = layerEightPos.x + 'px';
+
+    layerNinePos.x = (-mouse.x* 0.1) + (container2Pos.w/2 - layerNinePos.w / 2) ;
+    layerNine.style.left = layerNinePos.x + 'px';
+
+    layerTenPos.x = (-mouse.x* 0.08) + (container2Pos.w/2 - layerTenPos.w / 2) ;
+    layerTen.style.left = layerTenPos.x + 'px';
+
+    layerElevenPos.x = (-mouse.x* 0.04) + (container2Pos.w/2 - layerElevenPos.w / 2) ;
+    layerEleven.style.left = layerElevenPos.x + 'px';
+
     // layerOnePos.y = (-mouse.y*0.05) + (container1Pos.h / 2 - layerOnePos.h / 2)  ;
     // layerOne.style.top = layerOnePos.y + 'px';
 
@@ -59,20 +94,22 @@ function onMouseMove (evt) {
     // layerFive.style.top = layerFivePos.y + 'px';
 }
 
-window.addEventListener("scroll",onScroll);
-
-function onScroll (evt) {
-    for (i = 0; i < layerList.length; i++) {
-
-        // console.log(window.scrollY, layerList[i].offsetTop);
-        if (window.scrollY - 100 >= layerList[i].offsetTop) {
-            layerList[i].classList.add('fadeIn');
-            layerList[i].classList.remove('fadeOut');
-        } else {
-            layerList[i].classList.add('fadeOut');
-            layerList[i].classList.remove('fadeIn');
-        }
-    }  
 
 
-}
+// window.addEventListener("scroll",onScroll);
+
+// function onScroll (evt) {
+//     for (i = 0; i < layerList.length; i++) {
+
+//         // console.log(window.scrollY, layerList[i].offsetTop);
+//         if (window.scrollY - 100 >= layerList[i].offsetTop) {
+//             layerList[i].classList.add('fadeIn');
+//             layerList[i].classList.remove('fadeOut');
+//         } else {
+//             layerList[i].classList.add('fadeOut');
+//             layerList[i].classList.remove('fadeIn');
+//         }
+//     }  
+
+
+// }
